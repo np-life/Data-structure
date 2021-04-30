22e324ea9e74097d5cc0841d6dd4ea5afe9c1431
nplife <haklife47815@naver.com>
2021-05-01 01:21:02 +0900

    char * data = "Tuesday" 수정.

--- 06.List/리스트.cpp
+++ 06.List/리스트.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+# define BUFFER_SIZE 50
+
 struct node {
 	char* data;  // 데이터 필드.
 	struct node* next;  // 링크 필드.
@@ -11,17 +13,19 @@ Node* head = NULL;
 
 int main() 
 {
+	char tmp[BUFFER_SIZE] = "hello";
+
 	head = (Node*)malloc(sizeof(Node));
-	head->data = "Tuesday";
+	head->data = _strdup("Tuesday");
 	head->next = NULL;
 
 	Node * q = (Node*)malloc(sizeof(Node));
-	q->data = "Thursday";
+	q->data = _strdup(tmp);
 	q->next = NULL;
 	head->next = q;
 
 	q = (Node*)malloc(sizeof(Node));
-	q->data = "Monday";
+	q->data = _strdup("Monday");
 	q->next = head;
 	head = q;
 
